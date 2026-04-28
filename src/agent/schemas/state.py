432cd6548af1fc22ea401ff9ae4fd9b22bdacb14
Erick Silva <eriickhenriique.s.silva@gmail.com>
2026-04-27 22:42:20 -0300

    feat: adiciona a memória compartilhada de todos os agentes

---
+++ src/agent/schemas/state.py
@@ -0,0 +1,28 @@
+from operator import add
+from typing import Annotated, TypedDict
+
+from agent.schemas.analysis import FinancialAnalysis
+from agent.schemas.query import QueryPlan, RetrievedChunk
+
+class AgentState(TypedDict):
+    """Estado do agente durante o processo de análise financeira, contendo a pergunta original do usuário, 
+    o plano de consulta gerado, os chunks recuperados, a análise financeira rascunho gerada, a validade da resposta, 
+    erros de validação, contagem de tentativas, próximo agente a ser acionado, status de conclusão, 
+    e a resposta final após validação e possíveis iterações adicionais."""
+
+    user_query: str
+
+    query_plan: QueryPlan
+
+    retrieved_chunks: list[RetrievedChunk]
+
+    draft_analysis: FinancialAnalysis
+
+    is_valid: bool
+    validation_errors: Annotated[list[str], add]
+
+    retry_count: int
+    next_agent: str
+    is_done: bool
+
+    final_answer: FinancialAnalysis
